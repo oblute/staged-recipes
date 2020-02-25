3289152b58049c773d6ecb3ca35d2d96e7010db5
oblute <olivia.blute@gmail.com>
2020-02-25 11:13:24 -0500

    specify cuda versions in build.sh

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-export PATH=/usr/local/cuda
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\
+export PATH=/usr/local/cuda-10.2/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64\
          ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export CUDA_ROOT=/usr/local/cuda/include
 export CPATH=$CPATH:/usr/local/cuda/include
