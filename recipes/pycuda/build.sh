93e8b7d8c488f786af0a81541abddc2d75429670
oblute <olivia.blute@gmail.com>
2020-02-26 16:16:39 -0500

    edit cuda root

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -5,7 +5,7 @@ set -ex
 export PATH=/usr/local/cuda-10.2/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64\
          ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-export CUDA_ROOT=/usr/local/cuda/include
+export CUDA_ROOT=/usr/local/cuda
 export CPATH=$CPATH:/usr/local/cuda/include
 export CUDA_INC_DIR=/usr/local/cuda/bin:$CUDA_INC_DIR
 export NO_CUDA=1
