2d8c382cefeb1884db31d1108bf5cc58f11c1a3b
oblute <olivia.blute@gmail.com>
2020-02-28 10:00:56 -0500

    change CUDA_ROOT to CUDA_HOME

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -2,16 +2,13 @@
 
 set -ex
 
-platform='unknown'
-unamestr="$(uname)"
-
-if [[ "$unamestr" == 'Linux' ]]; then
-    platform='linux'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-    platform='freebsd'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    platform='osx'
-fi
+export PATH=/usr/local/cuda-10.2/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64\
+         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUDA_HOME=/usr/local/cuda/include
+export CPATH=$CPATH:/usr/local/cuda/include
+export CUDA_INC_DIR=/usr/local/cuda/bin:$CUDA_INC_DIR
+export NO_CUDA=1
 
 ./configure.py --cl-lib-dir=$PREFIX/lib --cl-libname=cuda
 
