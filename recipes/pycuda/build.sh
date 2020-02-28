fb2722f30c0ffb147ba5d817e9539e15ff9a58d9
oblute <olivia.blute@gmail.com>
2020-02-28 08:58:34 -0500

    this is foolish

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -13,6 +13,6 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
     platform='osx'
 fi
 
-./configure.py --cl-lib-dir=$PREFIX/lib --cl-libname=CUDA
+./configure.py --cl-lib-dir=$PREFIX/lib --cl-libname=cuda-gdb
 
 python setup.py install --single-version-externally-managed --record record.txt
