7822a788554ad97b0a19f8b89978b8295d84aee1
oblute <olivia.blute@gmail.com>
2020-12-03 10:05:22 -0500

    edit build script

--- recipes/ecctl/build.sh
+++ recipes/ecctl/build.sh
@@ -10,6 +10,6 @@ pushd cmd
 # Build
 go build -v -o ${PKG_NAME} .
 
-# Install Binary into PREFIX/bin
-mkdir -p $PREFIX/bin
-mv ${PKG_NAME} $PREFIX/bin/${PKG_NAME}
+# # Install Binary into PREFIX/bin
+# mkdir -p $PREFIX/bin
+# mv ${PKG_NAME} $PREFIX/bin/${PKG_NAME}
