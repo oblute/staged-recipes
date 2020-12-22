582b30496132689cfb6a79aafe08d09622c658fa
oblute <olivia.blute@gmail.com>
2020-12-22 14:37:45 -0500

    condense build script and remove unnecessary vars

--- recipes/mariadb-c/build.sh
+++ recipes/mariadb-c/build.sh
@@ -2,15 +2,7 @@ set -ex
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DINSTALL_HELPER_SCRIPTS=off \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_INSTALL_LIBDIR=lib \
-  -DBUILD_STATIC=OFF \
-  -DBUILD_TESTS=OFF \
-  -DBUILD_LENSTOOL=OFF \
-  -DBUILD_FOR_SSE=off \
-  -DBUILD_FOR_SSE2=off \
-  -DBUILD_DOC=OFF \
   .
 
 make -k -j${CPU_COUNT} || true
